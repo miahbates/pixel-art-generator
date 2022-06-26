f93ac9fbef39e832c83c5e396dce78ed9f8b2957
miahbates <miahbates@icloud.com>
2022-06-26 13:19:43 +0100

    first generative art

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // #1
+
+function setup() {
+  // set-up function runs on page load
+  createCanvas(windowWidth, windowHeight);
+  // loop over x1000
+  for (let i = 0; i < 1000; i++) {
+    // Inside the for loop, write code to draw a circles
+    // Fill(num red, num green, num blue, alpha (transparency))
+    fill(random(150), random(25), random(215), random(15));
+    // Elipse is coordinated for centtere of a circle ((x,y,width,h))
+    ellipse(random(windowWidth), random(windowHeight), random(50));
+  }
+}
